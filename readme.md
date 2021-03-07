59c5dde36f44491b61b09774a26d129b95ab1edc
Henrylle Maia <henryllemaia@gmail.com>
2021-03-07 18:00:57 -0300

    split de vídeo pra stories quando superior a 15 seg

--- readme.md
+++ readme.md
@@ -16,7 +16,7 @@
 # Resize Image em percent para Facebook
 
 ```
-./resize_image_em_percent_para_facebook.sh stage/IMG_5445.JPG 50 instagram_feed
+./resize_image_em_percent_para_facebook.sh stage/IMG_5445.JPG 50 instagram_feed 180
 ```
 | Parametros | Descricao |
 | ------ | ----------- |
@@ -29,11 +29,15 @@
 # Resize Video
 
 ```
-PREVIEW=true ./resize_video.sh stage/ads-01.mp4 facebook_feed
+Ex1: PREVIEW=true ./resize_video.sh stage/ads-01.mp4 facebook_feed
+```
+```
+Ex2: PREVIEW=false ./resize_video.sh stage/ads-01.mp4 instagram_stories 9
 ```
 | Parametros | Descricao |
 | ------ | ----------- |
 | param1 | Path do vídeo. |
-| param2 | Para qual formato será redimensionado. Opções válidas. facebook_feed, facebook_stories, instagram_feed, instragram_stories. |
+| param2 | Para qual formato será redimensionado. Opções válidas. facebook_feed, facebook_stories, instagram_feed, instragram_stories.  Obs: Quando for passado que é dos stories e o vídeo tiver mais de 15 seg, ele fará a segmentação do vídeo para cada 1 ter o tamanho médio de 8 segundos ou mais. No output o path será concatenado com _video_000x para saber qual a posição do vídeo. |
+| param3 | Caso seja stories e o vídeo tenha mais de 15 seg, você poderá passar um valor numérico que indica (em segundos) a posição que deverá haver o corte do vídeo para segmentação. |
 | PREVIEW    | Variável de ambiente, que caso seja setado para true irá gerar uma imagem no segundo 10 com um quadrado vermelho bem no centro da imagem para ver se setá centralizado. Obs: Não é obrigatório |
-| result    | Caso PREVIEW=false: Será gerado um vídeo na mesma pasta de origem com o nome no seguinte padrão: *nome do arquivo + o formato destino (Ex: facebook_feed) + resolucao (Ex: 1080x1080)*. Caso PREVIEW=true  Será gerado uma imagem na mesma pasta de origem com o nome no seguinte padrão: *nome do arquivo + **screenshot** + o formato destino (Ex: facebook_feed) + resolucao (Ex: 1080x1080)*. |
+| result    | Caso PREVIEW=false: Será gerado um vídeo na mesma pasta de origem com o nome no seguinte padrão: *nome do arquivo + o formato destino (Ex: facebook_feed) + resolucao (Ex: 1080x1080)*. Caso PREVIEW=true  Será gerado uma imagem na mesma pasta de origem com o nome no seguinte padrão: *nome do arquivo + **screenshot** + o formato destino (Ex: facebook_feed) + resolucao (Ex: 1080x1080)*. Atente para a obs no param2, caso seja stories. |
--- resize_video.sh
+++ resize_video.sh
@@ -2,6 +2,7 @@ video_path=$1
 crop_to=$2
 clean_temp_folder=true
 auto_cut_to_stories=true
+duracao_prevista_segmento=$3
 #name_video_path=$(basename $video_path)
 nome_file=$(echo $video_path | cut -d'.' -f 1)
 extensao=$(echo $video_path| cut -d'.' -f 2)
@@ -39,11 +40,18 @@ if [ "$crop_to" != "facebook_feed" ] && [ "$crop_to" != "facebook_stories" ] && 
   exit 1;
 fi
 
-if [ "$crop_to" != "facebook_stories" ] && [ "$crop_to" != "instagram_stories" ]; then
-  parametros_corte_stories="-segment_time 00:12:00"
-  exit 1;
-fi
+duracao_video=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $video_path)  
 
+#Videos maiores que 15 seg para stories precisam ser splitados
+if (( $(echo "$duracao_video > 15" | bc -l) )); then
+  if [ "$crop_to" == "facebook_stories" ] || [ "$crop_to" == "instagram_stories" ]; then      
+    if [ -z "$duracao_prevista_segmento" ]; then
+      duracao_prevista_segmento=8
+    fi  
+    parametros_corte_stories="-segment_time $duracao_prevista_segmento -f segment -reset_timestamps 1"
+    sequence_pattern="_video_%03d"  
+  fi
+fi
 
 height_original_video=$(ffprobe -v error -show_entries stream=height -of csv=p=0:s=x $video_path)
 width_original_video=$(ffprobe -v error -show_entries stream=width -of csv=p=0:s=x $video_path)
@@ -57,7 +65,7 @@ esac
 
 config_scale_video="crop=$width_new_video:$height_new_video:$crop_left:$crop_top"
 
-final_video_path="$nome_file""_"$texto_fonte_destino"_""$width_new_video""x""$height_new_video"".""$extensao"
+final_video_path="$nome_file""_"$texto_fonte_destino"_""$width_new_video""x""$height_new_video""$sequence_pattern.""$extensao"
 path_image_example="$nome_file""_screenshot_"$texto_fonte_destino"_""$width_new_video""x""$height_new_video"".jpg"
 
 
@@ -69,8 +77,13 @@ if [ ! -z $PREVIEW ] && [ $PREVIEW == "true" ]; then
   eog $path_image_example  
 else
   printf "Gerando vídeo para o formato $crop_to na resolucao $width_new_video:$height_new_video"
-  ffmpeg -i $video_path -vf "$config_scale_video" -c:a copy -c:v libx264 -crf 10 $final_video_path -y
+  ffmpeg -i $video_path -ss 00:00:00.2 -vf "$config_scale_video" -c:a copy -c:v libx264 -crf 10 $parametros_corte_stories $final_video_path -y
   check_error
   printf 'OK\n'  
-  ffplay $final_video_path
+  
+  if [ -z $duracao_prevista_segmento ]; then
+    ffplay $final_video_path
+  else
+    echo 'Vídeo segmentado não tem autoplay. Trabalho finalizado!'
+  fi
 fi
